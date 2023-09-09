0610ffba028da7cdc9b28c91742a50be224c0802
Quan Quan <79500388+QuanNguyenDong@users.noreply.github.com>
2023-09-09 21:26:08 +1000

    managing NATS client

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,12 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import request from "supertest";
-import { app } from "../app";
 import jwt from "jsonwebtoken";
 
 declare global {
     var signin: () => string[];
 }
+
+jest.mock("../nats-wrapper");
 
 let mongo: any;
 beforeAll(async () => {
@@ -20,6 +20,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+    jest.clearAllMocks();
     const collections = await mongoose.connection.db.collections();
 
     for (let collection of collections) {
@@ -51,7 +52,7 @@ global.signin = () => {
     const sessionJSON = JSON.stringify(session);
 
     // Take JSON and encode it as base64
-    const base64 = Buffer.from(sessionJSON).toString('base64');
+    const base64 = Buffer.from(sessionJSON).toString("base64");
 
     // return a string thats the cookie with the encoded data
     return [`session=${base64}`];
